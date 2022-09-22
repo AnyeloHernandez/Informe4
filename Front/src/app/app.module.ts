61ea04a6e6d4e79a8627d63157de4266c814be46
Anyelo Hernandez <anyelo.g.hernandez@gmail.com>
2022-09-21 21:38:36 -0600

    git fix

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -10,14 +10,11 @@ import { LoginComponent } from './components/login/login.component';
 
 import { LoginService } from './services/login.service';
 import { PublicacionesComponent } from './components/publicaciones/publicaciones.component';
-<<<<<<< HEAD
 import { PublicarComponent } from './components/publicar/publicar.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 
-=======
 import { RegisterComponent } from './components/register/register.component';
 import { ReactiveFormsModule } from '@angular/forms';
->>>>>>> 993dfc1fdbac617cbe928e2594a343e2fbd799c1
 
 
 
@@ -26,22 +23,16 @@ import { ReactiveFormsModule } from '@angular/forms';
     AppComponent,
     LoginComponent,
     PublicacionesComponent,
-<<<<<<< HEAD
     PublicarComponent,
-    NavigationComponent
-=======
+    NavigationComponent,
     RegisterComponent
->>>>>>> 993dfc1fdbac617cbe928e2594a343e2fbd799c1
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-<<<<<<< HEAD
-    FormsModule
-=======
+    FormsModule,
     ReactiveFormsModule
->>>>>>> 993dfc1fdbac617cbe928e2594a343e2fbd799c1
   ],
   providers: [
     LoginService
